0560c1659f19078fd59aabfada054879a75887ca
Dylan Waugh <dylwaugh@gmail.com>
2020-04-10 10:26:52 -0400

    small refactor add tests

--- pool_control/buttons/buttons.py
+++ pool_control/buttons/buttons.py
@@ -1,25 +1,32 @@
 
+onoff = ('on', 'off')
+spapooloff = ('spa', 'pool', 'off')
 
 class Buttons:
 
-    def __init__(self):
-        self._spa_pool_off_state = 'off'
-        self._pump_state = 'off'
+    def __init__(self, name, state='off'):
+        self._name = name
+        self._button_state = state
 
-    def set_spa_pool_off(self, value):
-        if value not 'on' or 'off':
+    def set_button_state(self, value):
+        if value not in onoff:
             raise ValueError
         else:
-            self._spa_pool_off_state = value
+            self._button_state = value
 
-    def pump(self, value):
-        if value not 'on' or 'off':
+    def button_state(self):
+        return self._button_state
+
+
+class SpaPoolButton(Buttons):
+
+    def __init__(self, name):
+        if name != 'spa_pool':
+            raise ValueError
+        super().__init__(name)
+
+    def set_button_state(self, value):
+        if value not in spapooloff:
             raise ValueError
         else:
-            self._pump_state = value
-
-    def pump_state(self):
-        return self._pump_state
-
-    def spa_pool_off_state(self):
-        return self._spa_pool_off_state
+            self._button_state = value
---
+++ pool_control/tests/test_buttons.py
@@ -0,0 +1,41 @@
+import pytest
+from buttons.buttons import *
+
+
+def test_buttons_are_off():
+    button = Buttons('pump')
+    assert button.button_state() == 'off'
+
+
+def test_buttons_are_on():
+    button = Buttons('pump')
+    button.set_button_state('on')
+    assert button.button_state() == 'on'
+
+
+def test_spa_button():
+    button = SpaPoolButton('spa_pool')
+    assert button.button_state() == 'off'
+
+
+def test_spa_button_pool():
+    button = SpaPoolButton('spa_pool')
+    button.set_button_state('pool')
+    assert button.button_state() == 'pool'
+
+
+def test_spa_button_sap():
+    button = SpaPoolButton('spa_pool')
+    button.set_button_state('spa')
+    assert button.button_state() == 'spa'
+
+
+def test_spa_button_raise():
+    with pytest.raises(ValueError):
+        SpaPoolButton('stuff')
+
+
+def test_spa_button_set_raise():
+    button = SpaPoolButton('spa_pool')
+    with pytest.raises(ValueError):
+        button.set_button_state('stuff')
